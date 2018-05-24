37e409b3f4dd86214e4245fa79368014de82ed0a
Sam Morrison <sammorrison2@me.com>
2018-05-24 11:45:41 +1000

    Game data load almost complete

--- javaSrc/PlayerDefinition.java
+++ javaSrc/PlayerDefinition.java
@@ -11,17 +11,22 @@ public class PlayerDefinition
   public String noseShape;
   public String faceShape;
 
-  public PlayerDefinition(String hairLength, String glasses, String facialHair, String eyeColor, String pimples, String hat, String hairColor, String noseShape, String faceShape)
-  {
+  public PlayerDefinition(String name, String hairLength, String glasses, String facialHair, String eyeColor, String pimples, String hat, String hairColor, String noseShape, String faceShape){
+    this.name = name;
     this.hairLength = hairLength;
     this.glasses = glasses;
     this.facialHair = facialHair;
     this.eyeColor = eyeColor;
-    this. pimples = pimples;
+    this.pimples = pimples;
     this.hat = hat;
     this.hairColor = hairColor;
     this.noseShape = noseShape;
     this.faceShape = faceShape;
+  }
+
+  public PlayerDefinition()
+  {
+
   }
 
 }
--- javaSrc/RandomGuessPlayer.java
+++ javaSrc/RandomGuessPlayer.java
@@ -7,14 +7,14 @@ import java.io.*;
  * You may implement/extend other interfaces or classes, but ensure ultimately
  * that this class implements the Player interface (directly or indirectly).
  */
-public class RandomGuessPlayer implements Player extends PlayerDefinition
+public class RandomGuessPlayer extends PlayerDefinition implements Player
 {
     // Each player needs to know who they are from the players list
     // which can be found through this.name as we extend PlayerDefinition
     //
     // They also need to hold a list of all other players in the game so
     // that they can eliminate players as the guesses happen(minus themselves)
-    private ArrayList<PlayerDefinition> players;
+    private Data gameData;
 
     // We need a list of all possible attributes and their values for this player to guess from, this could be the data/utility class with an accessible arraylist
     // of all attributes and values
@@ -37,11 +37,31 @@ public class RandomGuessPlayer implements Player extends PlayerDefinition
         //Assigns the name for this RandomGuessPlayer
         //Using that name we can then find which values to load
         //into this player
-        this.name = chosenName;
+        gameData = new Data();
+        // Load all the players and attributes in
+        gameData.loadGameFile(gameFilename);
 
+        // Copy the data of the player with the chosenName to this instance
+        // and remove it from the players list as we won't be guessing ourselves
+        loadSelf(gameData.getPlayerByName(chosenName));
 
     } // end of RandomGuessPlayer()
 
+    private void loadSelf(PlayerDefinition temp)
+    {
+        this.name = temp.name;
+        this.hairLength = temp.hairLength;
+        this.glasses = temp.glasses;
+        this.facialHair = temp.facialHair;
+        this.eyeColor = temp.eyeColor;
+        this.pimples = temp.pimples;
+        this.hat = temp.hat;
+        this.hairColor = temp.hairColor;
+        this.noseShape = temp.noseShape;
+        this.faceShape = temp.faceShape;
+        // Remove ourselves from the data set
+        gameData.removePlayerByName(this.name);
+    }
 
     public Guess guess() {
 
