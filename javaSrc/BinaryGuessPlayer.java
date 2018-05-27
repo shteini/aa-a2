d13a5e4fcf39137aff31fd4d205cd1f093409cde
Sam Morrison <sammorrison2@me.com>
2018-05-27 20:50:13 +1000

    Commented all code and verified that it is working

--- javaSrc/BinaryGuessPlayer.java
+++ javaSrc/BinaryGuessPlayer.java
@@ -9,140 +9,180 @@ import java.io.*;
  */
 public class BinaryGuessPlayer extends PlayerDefinition implements Player
 {
-    private Data gameData;
-    private AttributeCounter counter;
-    /**
-     * Loads the game configuration from gameFilename, and also store the chosen
-     * person.
-     *
-     * @param gameFilename Filename of game configuration.
-     * @param chosenName Name of the chosen person for this player.
-     * @throws IOException If there are IO issues with loading of gameFilename.
-     *    Note you can handle IOException within the constructor and remove
-     *    the "throws IOException" method specification, but make sure your
-     *    implementation exits gracefully if an IOException is thrown.
+  /* Each player needs to know who they are from the players list
+   * which can be found through this.name as we extend PlayerDefinition
+   *
+   * They also need to hold a list of all other players in the game so
+   * that they can eliminate players as the guesses happen
+   * I would have assumed that two players cannot be the samer person i.e.
+   * game1.chosen != P1 P1. for this reason, each player removes themselves
+   * from their own list. As this was not specified in the specs I am assuming that two players
+   * cannot be the same
+   */
+  private Data gameData;
+
+  // An instance of the AttributeCounter class to be used by BinaryGuessPlayer
+  private AttributeCounter counter;
+  /**
+   * Loads the game configuration from gameFilename, and also store the chosen
+   * person.
+   *
+   * @param gameFilename Filename of game configuration.
+   * @param chosenName Name of the chosen person for this player.
+   * @throws IOException If there are IO issues with loading of gameFilename.
+   *    Note you can handle IOException within the constructor and remove
+   *    the "throws IOException" method specification, but make sure your
+   *    implementation exits gracefully if an IOException is thrown.
+   */
+
+  public BinaryGuessPlayer(String gameFilename, String chosenName) throws IOException
+  {
+    gameData = new Data();
+
+    // Load all the players and attributes in
+    gameData.loadGameFile(gameFilename);
+
+    /* Assigns the name for this BinaryGuessPlayer
+     * Using that name we can then find which values to load
+     * into this player
      */
+    this.name = chosenName;
 
-    public BinaryGuessPlayer(String gameFilename, String chosenName)
-        throws IOException
+    // Copy the data of the player with the chosenName to this instance
+    loadSelf(gameData.getPlayerByName(chosenName));
+
+    /* The AttributeCounter is initialized with the gameData
+     * NB: this is the gameData that belongs to this BinaryGuessPlayer
+     */
+    counter = new AttributeCounter(gameData);
+
+    //Count all attribute key value pairs in the gameData
+    counter.countAttributes(gameData);
+  } // end of BinaryGuessPlayer()
+
+
+  public Guess guess()
+  {
+    // If there is only 1 player left the guess will be that player by name
+    // otherwise we get a random attribute/value pair and make a guess
+    if(gameData.players.size() == 1)
     {
-        //Load data in from file
-        //Assigns the name for this RandomGuessPlayer
-        //Using that name we can then find which values to load
-        //into this player
-        gameData = new Data();
-        // Load all the players and attributes in
-        gameData.loadGameFile(gameFilename);
-
-        // Copy the data of the player with the chosenName to this instance
-        // and remove it from the players list as we won't be guessing ourselves
-        this.name = chosenName;
-        loadSelf(gameData.getPlayerByName(chosenName));
-
-        //attribute counter
-        counter = new AttributeCounter(gameFilename, gameData);
-        counter.countAttributes(gameData);
-
-    } // end of BinaryGuessPlayer()
-
-
-    public Guess guess() {
-
-        if(gameData.players.size() == 1)
-        {
-            return new Guess(Guess.GuessType.Person, "", gameData.players.get(0).name);
-        }
-        else {
-            String[] guess = counter.binaryGuess(gameData.players.size());
-            return new Guess(Guess.GuessType.Attribute, guess[0], guess[1]);
-        }
-
-        // placeholder, replace
-
-    } // end of guess()
-
-    private void loadSelf(PlayerDefinition temp)
-    {
-        // For each key in the hashmap we get the key and use it to get
-        // the value from the hashmap and save it in this (this object)
-        for(String key: temp.attributes.keySet())
-        {
-            this.attributes.put(key, temp.attributes.get(key));
-        }
-        // Remove ourselves from the data set
-        gameData.removePlayerByName(this.name);
+      return new Guess(Guess.GuessType.Person, "", gameData.players.get(0).name);
     }
-
-
-      public boolean answer(Guess currGuess)
-      {
-        // If the current guestype is Attribute check to see if this player has the key and value
-        // if so return true as the guess was correct otherwise method will return false
-        if(currGuess.getType() == Guess.GuessType.Attribute)
-        {
-          String guessKey = currGuess.getAttribute();
-          String guessValue = currGuess.getValue();
-          if(this.attributes.containsKey(guessKey))
-          {
-            if(this.attributes.get(guessKey).equals(guessValue))
-            {
-              return true;
-            }
-          }
-        }
-        else
-        {
-          // if the guess type was person check the name against this instance, if they
-          // are equal return true, otherwise method will return false
-          String name = currGuess.getValue();
-          if(this.name.equals(name))
-          {
-            return true;
-          }
-        }
-        return false;
-      } // end of answer()
-
-
-    public boolean receiveAnswer(Guess currGuess, boolean answer)
+    else
     {
-      // If the guess type is person there are two cases
-      // 1 the person is correctly guessed in which case we return true
-      // 2 the person is incorrect. So we remove them from our Players list and
-      // return false
-      if(currGuess.getType() == Guess.GuessType.Person)
+
+     /* Here we call the binaryGuess method of AttributeCounter class and it returns
+      * a String array containing a key value pair which will attempt to halve the current population
+      * of characters left in the game. In the case that there is an odd number of players left
+      * this method will return a key value pair that is as close to half rounded down. i.e
+      * 9 players left, half 9 is 4.5, this gets rounded down to 4 and the binaryGuess finds an attribute
+      * key value pair that 4 players have we then get the key and value out of the
+      * string array and return it as part of the guess
+      */
+      String[] guess = counter.binaryGuess(gameData.players.size());
+      return new Guess(Guess.GuessType.Attribute, guess[0], guess[1]);
+    }
+  } // end of guess()
+
+  private void loadSelf(PlayerDefinition temp)
+  {
+    // For each key in the hashmap we get the key and use it to get
+    // the value from the hashmap and save it in this (this object)
+    for(String key: temp.attributes.keySet())
+    {
+        this.attributes.put(key, temp.attributes.get(key));
+    }
+    /* remove this instance from the players list as we are assuming that the other player cannot
+    * be the same player as us as stated above.
+    * NB: To allow players to choose the same person just comment out this line and the program will
+    * run as expected
+    */
+    gameData.removePlayerByName(this.name);
+  }
+
+
+  public boolean answer(Guess currGuess)
+  {
+    /* If the current guestype is Attribute check to see if this player has the key and value
+    * If it is present return true as the guess was correct
+    * if guessType is Attribute and the key value pair is not present, we return false
+    */
+    if(currGuess.getType() == Guess.GuessType.Attribute)
+    {
+      String guessKey = currGuess.getAttribute();
+      String guessValue = currGuess.getValue();
+      if(this.attributes.containsKey(guessKey))
       {
-        if(answer)
+        if(this.attributes.get(guessKey).equals(guessValue))
         {
           return true;
         }
-        else
-        {
-          gameData.removePlayerByName(currGuess.getValue());
-          counter.wipeCounter(gameData);
-          counter.countAttributes(gameData);
-          return false;
-        }
+      }
+    }
+    else
+    {
+      // if the guessType was person check the name against this instance, if they
+      // are equal return true, otherwise method will return false
+      String name = currGuess.getValue();
+      if(this.name.equals(name))
+      {
+        return true;
+      }
+    }
+    return false;
+  } // end of answer()
+
+
+  public boolean receiveAnswer(Guess currGuess, boolean answer)
+  {
+    /* If the guess type is person there are two cases
+     * 1 the person is correctly guessed in which case we return true
+     * 2 the person is incorrect. So we remove them from our Players list and
+     * return false
+     */
+    if(currGuess.getType() == Guess.GuessType.Person)
+    {
+      if(answer)
+      {
+        return true;
       }
       else
       {
-        // If GuessType is Attribute and answer is true, remove all players that dont have the guessed
-        // attribute.
-        // If answer is false remove all players that do have guessed attribute
-        if(answer)
-        {
-          gameData.removePlayersWithoutAttribute(currGuess.getAttribute(), currGuess.getValue());
-          counter.wipeCounter(gameData);
-          counter.countAttributes(gameData);
-        }
-        else
-        {
-          gameData.removePlayersWithAttribute(currGuess.getAttribute(), currGuess.getValue());
-          counter.wipeCounter(gameData);
-          counter.countAttributes(gameData);
-        }
+        gameData.removePlayerByName(currGuess.getValue());
+        // after removing the player we wipe the counter and re intialise it with the remaining
+        // players attributes by passing gameData to it
+        counter.wipeCounter(gameData);
+        // We then re count the attribute key value pairs
+        counter.countAttributes(gameData);
+        return false;
       }
-      return false;
-    } // end of receiveAnswer()
+    }
+    else
+    {
+      // If GuessType is Attribute and answer is true, remove all players that dont have the guessed
+      // attribute.
+      // If answer is false remove all players that do have guessed attribute
+      if(answer)
+      {
+        gameData.removePlayersWithoutAttribute(currGuess.getAttribute(), currGuess.getValue());
+        // after removing the players we wipe the counter and re intialise it with the remaining
+        // players attributes by passing gameData to it
+        counter.wipeCounter(gameData);
+        // We then re count the attribute key value pairs
+        counter.countAttributes(gameData);
+      }
+      else
+      {
+        gameData.removePlayersWithAttribute(currGuess.getAttribute(), currGuess.getValue());
+        // after removing the player we wipe the counter and re intialise it with the remaining
+        // players attributes by passing gameData to it
+        counter.wipeCounter(gameData);
+        // We then re count the attribute key value pairs
+        counter.countAttributes(gameData);
+      }
+    }
+    return false;
+  } // end of receiveAnswer()
 
 } // end of class BinaryGuessPlayer
--- javaSrc/Data.java
+++ javaSrc/Data.java
@@ -3,7 +3,9 @@ import java.io.*;
 
 public class Data
 {
+  // All possible attribute key value pairs
   public Map<String, List<String>> attributes;
+  // All players in the game
   public List<PlayerDefinition> players;
 
   public Data()
@@ -12,6 +14,10 @@ public class Data
     players = new ArrayList<PlayerDefinition>();
   }
 
+  /*
+   * returns a random attribute key value pair
+   * This method returns a random attribute key value pair
+   */
   public Map<String, String> getRandomAttribute()
   {
     // Empty Hashmap for the selected attribute and value
@@ -23,45 +29,59 @@ public class Data
     int randOne = randomGenerator.nextInt(attributes.keySet().size());
     String key = keys.get(randOne);
     int randTwo = randomGenerator.nextInt(attributes.get(key).size());
-    // Use the selected key to get the corresponding value
+    // Use the selected key to get the corresponding random value
     String value = attributes.get(key).get(randTwo);
+    //store the key value pair in attribute
     attribute.put(key, value);
-    // return the attribute to guess
     return attribute;
   }
 
+  /*
+   * @param key the key that is to be checked against all players
+   * @param the value of the passed in key, to be checked against all players
+   * This method removes all players that have the passed in attribute key value pair
+   */
   public void removePlayersWithAttribute(String key, String value)
   {
     Iterator itr = players.iterator();
-      while(itr.hasNext())
+    while(itr.hasNext())
+    {
+      PlayerDefinition currentPlayer = (PlayerDefinition)itr.next();
+      if(currentPlayer.attributes.containsKey(key))
       {
-        PlayerDefinition currentPlayer = (PlayerDefinition)itr.next();
-        if(currentPlayer.attributes.containsKey(key))
+        if(currentPlayer.attributes.get(key).equals(value))
         {
-          if(currentPlayer.attributes.get(key).equals(value))
-          {
-            itr.remove();
-          }
+          itr.remove();
         }
       }
+    }
   }
 
+  /*
+   * @param key the key that is to be checked against all players
+   * @param the value of the passed in key, to be checked against all players
+   * This method removes all players that do not have the passed in attribute key value pair
+   */
   public void removePlayersWithoutAttribute(String key, String value)
   {
     Iterator itr = players.iterator();
-      while(itr.hasNext())
+    while(itr.hasNext())
+    {
+      PlayerDefinition currentPlayer = (PlayerDefinition)itr.next();
+      if(currentPlayer.attributes.containsKey(key))
       {
-        PlayerDefinition currentPlayer = (PlayerDefinition)itr.next();
-        if(currentPlayer.attributes.containsKey(key))
+        if(!(currentPlayer.attributes.get(key).equals(value)))
         {
-          if(!(currentPlayer.attributes.get(key).equals(value)))
-          {
-            itr.remove();
-          }
+          itr.remove();
         }
       }
+    }
   }
 
+  /*
+   * @param name the name of the player you are searching for
+   * This method finds and returns the player with the name matching the passed in name
+   */
   public PlayerDefinition getPlayerByName(String name)
   {
     PlayerDefinition temp = new PlayerDefinition();
@@ -76,6 +96,10 @@ public class Data
     return temp;
   }
 
+  /*
+   * @param name the name of the player you are searching for
+   * This method finds and removes the player with the name matching the passed in name
+   */
   public void removePlayerByName(String name)
   {
     for(int i = 0; i < players.size(); i++)
@@ -87,6 +111,11 @@ public class Data
     }
   }
 
+  /*
+   * @param filename the name of the file to load
+   * This method loads all attributes and then all players from the file that
+   * matches the passed in filename
+   */
   public void loadGameFile(String filename)
   {
     try
@@ -107,46 +136,60 @@ public class Data
     }
   }
 
+  /*
+   * @param br the bufferedReader being used to read from the file in the loadGameFile Methd
+   * This method loads all attributes from the file opened by the loadGameFile method
+   */
   private void loadAttributes(BufferedReader br) throws IOException
   {
+    // The current line
     String line;
+    // Read line by line until you reach end of file or an empty line
     while((line = br.readLine()) != null && !(line.isEmpty()))
     {
       List<String> values = new ArrayList<String>();
       String[] attributes = line.split(" ");
+      // The first value will be the key
       String key = attributes[0];
+      // add all values for the key
       for(int i = 1; i<attributes.length; i++)
       {
         values.add(attributes[i]);
       }
+      // add the values and the key to the attributes instance variable
       this.attributes.put(key, values);
     }
   }
 
+  /*
+   * @param br the bufferedReader being used to read from the file in the loadGameFile Methd
+   * This method loads all players from the file opened by the loadGameFile method
+   */
   private void loadPlayers(BufferedReader br) throws IOException
   {
     String line;
+    // Read in each line until we reach end of file
     while((line = br.readLine()) != null)
     {
+      //if the line is empty, skip it, as we are now about to reach a new player
       if(line.isEmpty())
       {
         continue;
       }
       // Empty player object to fill
       PlayerDefinition newPlayer = new PlayerDefinition();
-   // If the line length is 2 it is a player name
+      // current line is the players name
       newPlayer.name = line;
-
+      // Read in each line until we reach an empty line or end of file
       while ((line = br.readLine()) != null && !(line.isEmpty()))
       {
-              String[] values = line.split(" ");
-              newPlayer.attributes.put(values[0], values[1]);
+        String[] values = line.split(" ");
+        // add the attribute key value pair to the current newPlayer.attributes
+        newPlayer.attributes.put(values[0], values[1]);
       }
+      // Add the new player to the players list
       players.add(newPlayer);
     }
   }
+
 }
-
-
-
-
